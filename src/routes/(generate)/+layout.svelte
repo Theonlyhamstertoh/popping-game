a58e935fbf4c5ce687be3b57865004a720fd356f
weeeeebo <75579372+Theonlyhamstertoh@users.noreply.github.com>
2024-02-03 20:05:01 -0800

    removed the navbar in +layout for (generate) folder

--- src/routes/(generate)/+layout.svelte
+++ src/routes/(generate)/+layout.svelte
@@ -3,10 +3,4 @@
 	import LucideArrowLeft from '~icons/lucide/arrow-left';
 </script>
 
-<nav class="py-2 shadow-sm bg-white relative flex items-center justify-center">
-	<Button href="/" variant="ghost" class="text-sm gap-2  text-gray-700 font-mono font-normal">
-		<img src="/favicon.png" class="size-10" alt="Meowrite Logo" />
-		<h2 class="font-medium text-xl">Meowrite</h2>
-	</Button>
-</nav>
 <slot />
