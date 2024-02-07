92162e51a595d4a3e501ff822d123eaf60379cfb
weeeeebo <75579372+Theonlyhamstertoh@users.noreply.github.com>
2024-02-06 16:49:35 -0800

    popping game

--- src/lib/components/pop.svelte
+++ src/lib/components/pop.svelte
@@ -1,22 +1,42 @@
 <script lang="ts">
 	import { cn } from '$lib/utils';
+	import { fade } from 'svelte/transition';
+	import LucidePartyPopper from '~icons/lucide/party-popper';
 
 	let clicked = false;
+	let counter = 5000;
+	let timeoutId: number;
 	export let audio: HTMLAudioElement;
 
 	const playAudio = () => {
+		if (clicked) return;
 		audio.currentTime = 0.96;
 		clicked = true;
 		audio.play();
+		const countdown = () => {
+			counter -= 1;
+			if (counter === 0) {
+				clicked = false;
+				counter = 100;
+			} else {
+				timeoutId = setTimeout(countdown, 1);
+			}
+		};
+
+		timeoutId = setTimeout(countdown, 1);
 	};
 </script>
 
-<button
-	class={cn(
-		'size-12 rounded-lg text-gray-900 bg-gray-900  cursor-pointer',
-		clicked && 'bg-transparent border border-gray-400'
-	)}
-	on:click={playAudio}
->
-	Pop
-</button>
+<div in:fade class={cn(' bg-gray-950 rounded-lg border', clicked && 'bg-white')}>
+	<button
+		class={cn(
+			'size-12 font-bold transition relative  text-gray-50 cursor-pointer',
+			clicked
+				? 'bg-small-light-dotted-blur text-gray-700 '
+				: 'active:scale-[175%] hover:scale-105 text-gray-50 bg-small-dotted-blur'
+		)}
+		on:click={playAudio}
+	>
+		{clicked ? counter : 'Pop'}
+	</button>
+</div>
--- src/routes/pop/+page.svelte
+++ src/routes/pop/+page.svelte
@@ -8,12 +8,21 @@
 	onMount(() => {
 		audio = new Audio('pop.m4a');
 	});
+
+	const totalButtons = 120;
+	// Add numbers to the ones you clicked. so you have an order
+	// after clicking through all of them, regenerate them
+	// Make it game loopable
+	// Generate them in random spots
+	// Create a leaderboard with a name to attempt
 </script>
 
-<div class="grid grid-cols-6 w-fit mt-8 h-fit gap-2 mx-auto justify-center items-center">
-	{#each [...new Array(30)] as _}
-		<Pop {audio} />
-	{/each}
+<div class=" h-screen">
+	<div class="grid grid-cols-12 w-fit mt-8 h-fit gap-2 mx-auto justify-center items-center">
+		{#each [...new Array(totalButtons)] as _}
+			<Pop {audio} />
+		{/each}
+	</div>
 </div>
 
 <!--  -->
